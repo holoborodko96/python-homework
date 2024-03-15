57fa5be4188660bef99637e9ba90465322e1693f
holoborodko96 <holoboleks62214@gmail.com>
2024-03-15 17:53:28 -0400

    Lesson 5 hw

--- lesson_5/homework.py
+++ lesson_5/homework.py
@@ -12,7 +12,11 @@
 # Example:
 # Input: -3 => Output: 3
 # Input: 5 => Output: 5
-
+my_number = int(input("Enter your number: "))
+if my_number < 0:
+    print(my_number * -1)
+else:
+    print(my_number)
 
 # ---------------------------------------------------------------------
 
@@ -23,7 +27,15 @@
 # If the number is divisible of 7, print “Go”
 # For numbers which are divisible of 3 and 7, print “BinGo”
 # Otherwise, print the original number: “{number} is just a number”
-
+number = float(input("Enter your number: "))
+if number % 3==0 and number %7==0:
+    print("BinGo")
+elif number % 3 ==0:
+    print("Bin")
+elif number % 7 ==0:
+    print("Go")
+else:
+    print(f"{number} is just a number")
 
 # ---------------------------------------------------------------------
 
@@ -35,6 +47,17 @@
 #
 # Example:
 # x = 1, y = 5, z = 3 => Output: 3
+x = int(input("Enter X: "))
+y = int(input("Enter Y: "))
+z = int(input("Enter Z: "))
+if y < x < z or z < x < y:
+    print(f"{x}")
+elif x < y < z or z < y < x:
+    print(f"{y}")
+elif x < z < y or y < z < x:
+    print("z")
+else:
+    print("some numbers are equal")
 
 
 # ---------------------------------------------------------------------
@@ -47,7 +70,13 @@
 # It should print True if the number is a palindrome and False if it is not.
 #
 # Palindrome number: 121, 898
+entered_number = int(input("Enter your number: "))
+number = str(entered_number)
 
+if number[0]==number[-1]:
+    print("TRUE")
+else:
+    print("FALSE")
 
 # ---------------------------------------------------------------------
 
@@ -62,3 +91,8 @@
 #
 # Example: "tcefreP!" -> Perfect!
 
+entered_review = input("Enter wrong review: ")
+review = str(entered_review)
+right_review = review [: : -1]
+
+print(f"{right_review}")
